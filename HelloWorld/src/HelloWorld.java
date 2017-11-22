a76a9a43981e3be43e03c96dd446fb4226aab06c
Sushant Attada <sushant@purnatva.com>
2017-11-22 19:02:17 +0100

    First changes

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 10; 
+		int y = 20;
+		int z;  
+		
+		z = x + y ;
+		System.out.println("THe value of X + Y is: " +z);
+	}
+
+}
